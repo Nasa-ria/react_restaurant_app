71ef26c2af9de5efd228f25fb121c0aa59edb980
ria-hue <mumuninasaria@gmail.com>
2023-01-16 16:18:08 +0000

    worked on adding icons to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,19 @@ function Footer(){
     return(
         <>
      
-     <footer class="footer fixed-bottom bg-dark ">
-     <div class="container">
-     <p>footer</p>
- </div>
+     <footer class="footer  fixed-bottom bg-dark ">
+     <div class="d-flex flex-wrap justify-content-between align-items-center ">
+    <div >
+      <span class="mb-3 mb-md-0 text-muted">&copy; 2022 Company, Inc</span>
+    </div>
+
+    <ul class="nav col-md-2 justify-content-end list-unstyled d-flex gap-3 ">
+    <i class="bi bi-facebook" style={{color:"white"}}></i> 
+    <i class="bi bi-instagram" style={{color:"white"}}></i>
+ 
+    </ul>
+  </div>
+
     </footer> 
 
 
